4584b127e7b99bf019fae1259c0421cfe000ba3a
helloyello34 <kristinn34@gmail.com>
2017-11-28 23:59:40 +0000

    28/11/2017

--- main_assignment/include/Pizza.h
+++ main_assignment/include/Pizza.h
@@ -1,16 +1,26 @@
 #ifndef PIZZA_H
 #define PIZZA_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class Pizza
 {
     public:
         Pizza();
         virtual ~Pizza();
-
-    protected:
-
+        string get_name();
+        void print_ingr();
+        void create_pizza();
+        friend ostream& operator << (ostream& out, const Pizza pizza);
     private:
+        int id;
+        int price;
+        vector<string>alegg;
+        string name;
 };
 
 #endif // PIZZA_H
--- main_assignment/src/Pizza.cpp
+++ main_assignment/src/Pizza.cpp
@@ -2,10 +2,37 @@
 
 Pizza::Pizza()
 {
-    //ctor
+    this->name = "";
+    this->id = 0;
+    this->price = 0;
 }
 
 Pizza::~Pizza()
 {
     //dtor
+}
+
+string Pizza::get_name()
+{
+    return this->name;
+}
+
+void Pizza::print_ingr()
+{
+    for(int i = 0; i < this->alegg.size(); i++){
+        cout << this->alegg[i] << endl;
+    }
+}
+
+void Pizza::create_pizza()
+{
+
+}
+
+ostream& operator << (ostream& out, const Pizza pizza)
+{
+
+    out << pizza.id << ": " << pizza.name << endl;
+
+    return out;
 }
