91a37eac11bd3694045cd1becbfa96e5ddf98209
helloyello34 <kristinn34@gmail.com>
2017-12-14 15:08:57 +0000

    Service comment

--- main_assignment/src/Service/PizzaService.cpp
+++ main_assignment/src/Service/PizzaService.cpp
@@ -7,16 +7,19 @@ PizzaService::PizzaService()
 
 void PizzaService::add_pizza(Pizza& newPizza)
 {
+    // Call to the repository class to add the new pizza to the file
     pizza_repo.add_pizza(newPizza);
 }
 
 void PizzaService::retreve_pizza(vector<Pizza>&pizzas)
 {
+    // Call to the repository class to read data to the vector
     pizza_repo.retreve_pizza(pizzas);
 }
 
 void PizzaService::overwrite_pizza(vector<Pizza>&pizzas)
 {
+    // Call to the repository class to write over exsisting data
     pizza_repo.overwrite_pizza(pizzas);
 }
 
