1ca24195d10813d8bc0075596dbf6fc481f56e96
osskari <oskarjorgen@gmail.com>
2017-12-14 15:38:27 +0000

    comments yay

--- main_assignment/src/Model/Sides.cpp
+++ main_assignment/src/Model/Sides.cpp
@@ -2,6 +2,7 @@
 
 Sides::Sides()
 {
+    // Default initializer for sides
     strcpy(this->name, "nothing");
     this->price = 0;
 }
@@ -9,28 +10,33 @@ Sides::Sides()
 
 void Sides::write(ofstream& file)
 {
+    // Writes sides to file
     file.write(this->name, sizeof(this->name));
     file.write((char*)(&this->price), sizeof(this->price));
 }
 
 void Sides::read(ifstream& file)
 {
+    // Reads sides from file
     file.read(this->name, sizeof(this->name));
     file.read((char*)(&this->price), sizeof(this->price));
 }
 
 string Sides::get_name() const
 {
+    // Returns name of sides
     return (string)this->name;
 }
 
 double Sides::get_price() const
 {
+    // Returns price
     return this->price;
 }
 
 ostream& operator << (ostream& out, const Sides& side)
 {
+    // Print out sides
     out << "     Name: " << side.name << endl;
     out << "     Price: " << side.price << endl;
     return out;
@@ -38,11 +44,14 @@ ostream& operator << (ostream& out, const Sides& side)
 
 istream& operator >> (istream& in, Sides& side)
 {
+    // Input sides
     cout << "  Name: ";
     in.getline(side.name, 32);
+    // If name is length 0. Throw a fault
     if(strlen(side.name) == 0){
         throw InvalidNameException();
     }
+    // If name is not alphanumeric or a space. Throw a fault
     for(unsigned int i = 0; i < strlen(side.name); i++){
         if(!(isalnum(side.name[i]))){
             if(side.name[i] != ' '){
@@ -52,10 +61,12 @@ istream& operator >> (istream& in, Sides& side)
     }
     cout << "  Price: ";
     in >> side.price;
+    // if price is not a number. throw a fault
     if(in.fail()){
         in.clear();
         throw InvalidPriceException();
     }
+    // Price cap for sides
     if(side.price > 5000 || side.price < 0){
         throw InvalidPriceException();
     }
--- main_assignment/src/Model/Topping.cpp
+++ main_assignment/src/Model/Topping.cpp
@@ -2,12 +2,14 @@
 
 Topping::Topping()
 {
+    // Default initializer for toppings
     strcpy(name,"Nothing");
     this->price = 0;
 }
 
 Topping::Topping(char* name, double price)
 {
+    // Initializer with a set name and price
     strcpy(this->name, name);
     this->price = price;
 }
@@ -32,6 +34,7 @@ void Topping::write(ofstream& file)
 
 bool Topping::valid_topping() const
 {
+    // Checks if name of topping is valid
     if(strcmp(this->name, "nothing")) {
         return true;
     } else {
@@ -41,29 +44,34 @@ bool Topping::valid_topping() const
 
 string Topping::get_name()
 {
+    // Returns name
     return (string)this->name;
 }
 
 double Topping::get_price()
 {
+    // Returns price
     return this->price;
 }
 
 ostream& operator << (ostream& out, const Topping& topping)
 {
+    // prints out topping
     out << topping.name << " | " << topping.price << " kr.";
     return out;
 }
 
 istream& operator >> (istream& in, Topping& topping)
 {
-    //cout << "   Add Topping" << endl;
+    // input topping
     cout << " ======================" << endl;
     cout << "  Name : ";
     in.getline(topping.name, 32);
+    // If name is length 0. Throw a fault
     if(strlen(topping.name) == 0) {
         throw InvalidNameException();
     }
+    // If name is not alphanumeric or a space. Throw a fault
     for(size_t i = 0; i < strlen(topping.name); i++) {
         if(!(isalpha(topping.name[i]))){
             if(!(topping.name[i] == ' ')) {
@@ -75,10 +83,12 @@ istream& operator >> (istream& in, Topping& topping)
     cout << "  Price: ";
     in >> topping.price;
 
+    // If price is not a number. Throw a fault
     if(in.fail()) {
         in.clear();
         throw InvalidPriceException();
     }
+    // Price cap for toppings
     if(topping.price < 0 || topping.price > 5000) {
         throw InvalidPriceException();
     }
